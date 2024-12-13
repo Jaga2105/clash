c659461b60968c37f3d1240d01fb361fb83a7d1c
2266901_Jagannath <jagannathnayak199@gmail.com>
2024-12-13 13:19:30 +0530

    server setup done

--- server/index.js
+++ server/index.js
@@ -1,2 +1,12 @@
-console.log("Started")
-console.log("Initiated")
+import express from 'express'
+import 'dotenv/config'
+
+const app = express()
+const port = process.env.PORT || 7000
+
+app.get('/', (req,res)=>{
+    res.send(`Server is listening on port ${port}`)
+})
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`)
+})
